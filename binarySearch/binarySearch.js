12356369a53100cf1958dd36aee9be0c85199903
JuanCeja <jmc347ls2@yahoo.com>
2022-08-19 17:40:55 -0700

    added comments for better understanding

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -24,22 +24,30 @@ let nums2 = [-1, 0, 3, 5, 9, 12];
 let target2 = 2;
 
 var search = function (nums, target) {
+    // setting our variables
     let left = 0;
     let right = nums.length - 1;
     let mid;
 
+    // using while loop
     while (left <= right) {
+        // finding the middle of the array for the binary search
         mid = Math.floor((left + right) / 2);
 
+        // if the middle is equals to the target return the target
         if (nums[mid] === target) return mid;
 
+        // if the middle is less than the target then set the left to middle and add 1 to continue our next search. we add one because we already checked middle in the first 'if' statement
         if (nums[mid] < target) {
             left = mid + 1;
+
+            // if the middle is greater than the target then set the right to middle and add 1 to continue our next search. we add one because we already checked middle in the first 'if' statement
         } else {
             right = mid - 1;
         }
     }
 
+    // if we never find our target then just return -1
     return -1;
 }
 
