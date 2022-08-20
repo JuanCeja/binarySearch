43c87d9a13f9f05bf172d61f83305a386a688714
JuanCeja <jmc347ls2@yahoo.com>
2022-08-19 17:37:06 -0700

    complete

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -17,14 +17,30 @@
 
 // ========================================================================================
 
-let nums = [-1,0,3,5,9,12];
+let nums = [-1, 0, 3, 5, 9, 12];
 let target = 9;
 
-let nums2 = [-1,0,3,5,9,12];
+let nums2 = [-1, 0, 3, 5, 9, 12];
 let target2 = 2;
 
-var search = function(nums, target) {
-    
+var search = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    let mid;
+
+    while (left <= right) {
+        mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) return mid;
+
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
 }
 
 console.log(search(nums, target));
